0ba607ae905b2a3b1bec9b5ce84359e0d7a905c8
Anton Bentzen <ambentzen@me.com>
2012-09-14 16:15:51 +0200

    Operator overloading for Vector class
    Added that basic operator overloads for a vector.

--- src/Vector.hpp
+++ src/Vector.hpp
@@ -5,6 +5,14 @@ public:
 	Vector(double x, double y, double z);
 	Vector(const Vector &rhs);
 
-private:
+	Vector &operator+=(const Vector &rhs);
+	Vector &operator-=(const Vector &rhs);
+	Vector &operator*=(double rhs);
+	Vector &operator/=(double rhs);
+	const Vector operator+(const Vector &rhs);
+	const Vector operator-(const Vector &rhs);
+	const Vector operator*(double rhs);
+	const Vector operator/(double rhs);
+
 	double x, y, z;
 };
