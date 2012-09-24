a951f4e24bc1a016aab416d90962341a125a5ad1
Anton Bentzen <ambentzen@me.com>
2012-09-24 22:20:11 +0200

    Updated Vector test
    Vector test now uses the new test macros.

--- test/vector.cpp
+++ test/vector.cpp
@@ -1,9 +1,15 @@
 #include "Vector.hpp"
+#include "pigeon_test.hpp"
 #include <iostream>
+
+TEST_SETUP();
 
 int main(int argc, char *argv[]) {
 	Vector vec(0,0,0);
 	std::cout << "Hello test" << std::endl;
 
-	return 0;
+	ASSERT(vec.x, 0, "x is not 0.");
+
+	REPORT();
+	return ERROR_COUNT;
 }
