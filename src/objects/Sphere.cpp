d876acc2231d2d36fd41094831329b5c6f3d6508
Anton Bentzen <ambentzen@me.com>
2012-09-25 21:43:22 +0200

    Sphere class
    Basic Sphere class added, with dummy intersect method

---
+++ src/objects/Sphere.cpp
@@ -0,0 +1,12 @@
+#include "Sphere.hpp"
+#include "../Vector.hpp"
+#include "../Ray.hpp"
+
+Sphere::Sphere(const Vector &_center, double _radius) : center(_center), radius(_radius) {
+}
+
+bool Sphere::intersect(const Ray &ray) {
+	// find minimum distance
+	// compare to radius
+	return false;
+}
---
+++ src/objects/Sphere.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "Object.hpp"
+#include "../Ray.hpp"
+#include "../Vector.hpp"
+
+class Sphere : public Object {
+public:
+	Sphere(const Vector &center, double radius);
+
+	virtual bool intersect(const Ray &ray);
+
+private:
+	Vector center;
+	double radius;
+};
