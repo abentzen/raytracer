6a4f02ae35ac6c57b12908ce04b02ca1042bb8f1
Anton Bentzen <ambentzen@me.com>
2012-09-27 22:30:49 +0200

    Intersection of Sphere
    Implemented the intersection method of Sphere and added tests

--- src/objects/Sphere.cpp
+++ src/objects/Sphere.cpp
@@ -1,6 +1,7 @@
 #include "objects/Sphere.hpp"
 #include "Vector.hpp"
 #include "Ray.hpp"
+#include <cmath>
 
 Sphere::Sphere(const Vector &_center, double _radius) : center(_center), radius(_radius) {
 }
@@ -8,5 +9,24 @@ Sphere::Sphere(const Vector &_center, double _radius) : center(_center), radius(
 bool Sphere::intersect(const Ray &ray) {
 	// find minimum distance
 	// compare to radius
-	return false;
+	Vector c_o = center - ray.origin;
+
+	// If the ray is inside the sphere intersection is impossible.
+	if (c_o.length() < radius)
+		return false;
+
+	double len = c_o.length() * (c_o.dot(ray.direction)/(c_o.length()));
+
+	// If the closest point is "behind" the origin of the ray it cannot intersect.
+	if (len < 0)
+		return false;
+
+	Vector c_proj = ray.origin + (ray.direction * len);
+
+	double dist = (center - c_proj).length();
+
+	if (dist > radius)
+		return false;
+	else
+		return true;
 }
--- test/sphere.cpp
+++ test/sphere.cpp
@@ -9,10 +9,16 @@ int main(int argc, char *argv[]) {
 	Vector orig(0,0,0);
 	double radius = 1;
 
-	Ray ray(Vector(-2, 1, 0), Vector(1,0,0));
+	Ray ray1(Vector(-2, 1, 0), Vector(1,0,0));
+	Ray ray2(Vector(-2, 1, 0), Vector(-1,0,0));
+	Ray ray3(Vector(0, 0.5f, 0), Vector(1,0,0));
 	Sphere sphere(orig, radius);
 
-	ASSERT(sphere.intersect(ray), true, "No intersection");
+	ASSERT(sphere.intersect(ray1), true, "No intersection");
+
+	ASSERT(sphere.intersect(ray2), false, "Error in intersection");
+
+	ASSERT(sphere.intersect(ray3), false, "Origin is inside the Sphere");
 
 	REPORT();
 	return ERROR_COUNT;
